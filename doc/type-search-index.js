326317a1fe2a61b4217660726cf066424d65b0c7
ClaseJavaGetafe <Tardes@GET-04>
2025-09-11 19:20:20 +0200

    Clase peliculas con get set,constructor completo y metodo mostrarPelicula

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"grupotres.trebejos","l":"MenuSalas"},{"p":"grupotres.trebejos","l":"Pelicula"}];updateSearchResults();
