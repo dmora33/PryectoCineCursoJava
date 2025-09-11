b12370e5c328821b97615a929757d21f5ac91a38
Tardes <Tardes@GET-03>
2025-09-11 19:29:13 +0200

    Jueves 09/11/2025 Setters & Getters Clase SalaDeCine

--- src/grupotres/trebejos/SalaDeCine.java
+++ src/grupotres/trebejos/SalaDeCine.java
@@ -9,6 +9,41 @@ public class SalaDeCine {
 	private String butacas[][];
 	
 	// SETTERS & GETTERS
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public int getCapacidad() {
+		return capacidad;
+	}
+	
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+	
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+	
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+	
+	public String[][] getButacas() {
+		return butacas;
+	}
+	
+	public void setButacas(String[][] butacas) {
+		this.butacas = butacas;
+	}
+	
+	// MÉTODOS NO ESTÁTICOS
+	
+	
 	
 
 }
