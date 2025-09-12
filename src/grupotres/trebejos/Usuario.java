d017a7804464995abe6d0bd4d869f32ebbb67f60
ClaseJavaGetafe <Tardes@GET-04>
2025-09-12 18:32:59 +0200

    subido incompleto

--- src/grupotres/trebejos/Usuario.java
+++ src/grupotres/trebejos/Usuario.java
@@ -1,5 +1,7 @@
 package grupotres.trebejos;
 
+import java.util.Scanner;
+
 public class Usuario {
 
 	private String nombre;
@@ -29,9 +31,33 @@ public class Usuario {
 	public void muestraUsuario() {
 		System.out.println("NOMBRE: " + nombre + "\nEDAD: " + edad + "\nSaldo: " + saldo);
 	}
-	public void inicioUsuario() {
+
+	public Usuario inicioUsuario() {
+		System.out.println("Bien benido!\nVamos a pedirte tus datos personales;\n \nEscribe tu nónmbre:");
+		Scanner scan = new Scanner(System.in);
+		String nombre = scan.next();
 		
-		// TODO Usuario usuario = new Usuario(nombre, edad, saldo)
+		// primero empezamos con las validaciones de los datos NOMBRE
+		nombre= nombre.strip();
+		System.out.println("Escribre tu edad:");
+		scan = new Scanner(System.in);
+		int edad = scan.nextInt();
+		while ( edad<0 && edad>120) {
+			// TODO FALTA añadir que no meta caracteres solo numeros.
+			System.err.println("edad no valida, vuelve a introduzirla");
+			System.out.print("\nEscribre tu edad:");
+			scan = new Scanner(System.in);
+			edad = scan.nextInt();
+		}
+		System.out.println("Cuanto saldo quieres vas a añadir a tu cuenta:");
+		scan = new Scanner(System.in);
+		int saldo = scan.nextInt();
+		if (saldo=<0) { 
+			// TODO por acabar
+			
+			
+		}
+		Usuario usuario = new Usuario(nombre, edad, saldo);
 		
 		System.out.println("Dime tu nombre;");
 		
